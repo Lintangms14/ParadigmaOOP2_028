59a79d2560698d1b631927971fd14eb5ac072f2c
Lintangms14 <merayusukmalintang@gmail.com>
2023-05-31 19:50:46 +0700

    class pelajar, private manusia, public orang

--- MultiWaris/MultiWaris.cpp
+++ MultiWaris/MultiWaris.cpp
@@ -28,3 +28,22 @@ public:
 	}
 };
 
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string psekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(psekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + " dengan jenis kelamin "
+			+ jenisKelamin + " dari sekolah " + sekolah + "\n\n";
+	}
+};
+
